40c96fffea5aeae681bc781bad480514a2be1bb6
Yoan <yoan16908@gmail.com>
2026-01-20 15:31:45 -0500

    Fix vercel plugin incompatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
